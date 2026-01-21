df54fc00867ca3d49336f1b373ee7a8d27a7d9ec
Maps67 <marcoaps67@hotmail.com>
2026-01-20 21:58:18 -0600

    Fix & Refactor: Restauración total de integridad lógica + Implementación final Mobile Zero-Scroll con estética Glassmorphism

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.oa6jmar99p8"
+    "revision": "0.trc5vti5o0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
