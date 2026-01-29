90ca2a9ee67524b85b1e366e3e27077d0524a063
Maps67 <marcoaps67@hotmail.com>
2026-01-28 21:48:34 -0600

    SNAPSHOT: Sistema Estabilizado - Punto de Retorno Seguro

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.v4k3gcbp7og"
+    "revision": "0.dbtodn6ico8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
