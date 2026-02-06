2a98543475e1389cfa62d4f9bac1d05465fcf159
Maps67 <marcoaps67@hotmail.com>
2026-02-05 22:42:01 -0600

    Test de conexion: Actualizando titulo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.aol18ji65ts"
+    "revision": "0.b1p5qg4knes"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
